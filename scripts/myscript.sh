4c2079d00c026345da48390e87b45a429a4e0424
reddy <linux@localhost.localdomain>
2018-04-11 19:59:18 +0530

    add jenkinsfile

---
+++ scripts/myscript.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#purpose : to learn shell scripting
+#version : 1.0
+#owner   : linux@gmail.com
+#input   : none
+#output  : none
+
+#print welcome message
+ echo "welcome to bash scripting"
+#print end message
+ echo "end"
